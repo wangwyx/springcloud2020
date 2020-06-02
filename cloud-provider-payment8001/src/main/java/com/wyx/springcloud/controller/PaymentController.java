df92b996bb93934357b0d918aff582e43d22bef7
wangwyx <wangongxinwyx@163.com>
2020-06-02 12:49:58 +0800

    第四次提交：consule注册中心

--- cloud-provider-payment8001/src/main/java/com/wyx/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/wyx/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.wyx.springcloud.entities.Payment;
 import com.wyx.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @auth: wangyx-p
@@ -19,6 +22,9 @@ import javax.annotation.Resource;
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -46,4 +52,18 @@ public class PaymentController {
             return new CommonResult(444, "没有对应记录,查询ID: " + id + "serverPort:" + serverPort, null);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
 }
