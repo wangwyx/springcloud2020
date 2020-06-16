f5d70d9c713cbd3c621514a48a56a3e55c4bf382
wangwyx <wangongxinwyx@163.com>
2020-06-16 12:57:12 +0800

    增加gateway动态路由配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/wyx/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.wyx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @auth: wangyx-p
+ * @date :2020-6-13 13:53
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/wyx/springcloud/config/GateWayConfig.java
@@ -0,0 +1,30 @@
+package com.wyx.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @auth: wangyx-p
+ * @date :2020-6-15 12:48
+ * @Description:
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_wyx", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator1(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_wyx1", r -> r.path("/guonei").uri("http://news.baidu.com/guoji")).build();
+
+        return routes.build();
+    }
+}
